5e3a62b18f3218fa20c81c19c5ce0cbabe053c6c
= <=>
2022-02-23 08:24:20 +0100

    change reference to universal instead only user

--- src/sessions/session.entity.ts
+++ src/sessions/session.entity.ts
@@ -2,8 +2,8 @@ import { Field, ObjectType } from "@nestjs/graphql"
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
 import { Document } from "mongoose"
 import { MongooseIdProp } from "src/common/decorators/mongoose/id-prop.decorator"
+import { MongooseIdReference } from "src/common/decorators/mongoose/id-reference.prop"
 import { MongooseCreatedAtProp } from "src/common/decorators/mongoose/timestamps.decorator"
-import { MongooseUserIdProp } from "src/common/decorators/mongoose/user-id-prop.decorator"
 
 export type SessionDocument = Document & Session
 
@@ -19,7 +19,7 @@ export class Session {
   userAgent!: string
 
   @Field()
-  @MongooseUserIdProp()
+  @MongooseIdReference()
   userId!: string
 
   @Field()
