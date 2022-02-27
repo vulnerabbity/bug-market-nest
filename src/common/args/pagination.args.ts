badeb11e4b214bcd1a01688e2ab7c8fc84db130f
Your Name <you@example.com>
2022-02-27 14:32:10 +0700

    add pagination validation

--- src/common/args/pagination.args.ts
+++ src/common/args/pagination.args.ts
@@ -1,10 +1,15 @@
 import { ArgsType, Field } from "@nestjs/graphql"
+import { IsInt, Max, Min } from "class-validator"
 
 @ArgsType()
 export class PaginationArgs {
   @Field()
+  @Min(0)
   offset: number = 0
 
+  @IsInt()
+  @Max(50)
+  @Min(1)
   @Field()
   limit: number = 10
 }
