a1f3438f887ba52a0e359b6a984539d69132820b
Your Name <you@example.com>
2022-03-02 18:47:56 +0700

    add image uploading abilities

--- src/auth/authorization/abilities/casl-ability.factory.ts
+++ src/auth/authorization/abilities/casl-ability.factory.ts
@@ -1,22 +1,40 @@
-import { InferSubjects, Ability, AbilityBuilder } from "@casl/ability"
+import {
+  InferSubjects,
+  Ability,
+  AbilityBuilder,
+  ExtractSubjectType,
+  AbilityClass
+} from "@casl/ability"
 import { Injectable } from "@nestjs/common"
 import { User, UserRole } from "src/users/user.entity"
 import { RolesOwner } from "./abilities.interface"
 
-type Subject = InferSubjects<typeof User> | "all"
+type Subjects = InferSubjects<typeof User> | "all"
 
 export type Action = "manage" | "create" | "read" | "update" | "delete"
 
-export type AppAbility = Ability<[Action, Subject]>
+export type AppAbility = Ability<[Action, Subjects]>
 
 @Injectable()
 export class CaslAbilityFactory {
+  constructor() {}
   createForRolesOwner(rolesOwner: RolesOwner) {
-    const { can, cannot, build } = new AbilityBuilder<AppAbility>(Ability)
+    const { can, cannot, build } = new AbilityBuilder<Ability<[Action, Subjects]>>(
+      Ability as AbilityClass<AppAbility>
+    )
 
     const hasRole = this.createRoleChecker(rolesOwner)
 
-    return build()
+    if (hasRole("admin")) {
+      can("manage", "all")
+    }
+    if (hasRole("seller")) {
+      can("manage", User, { id: rolesOwner.id }).because("You can only can manage yourself")
+    }
+
+    return build({
+      detectSubjectType: item => item.constructor as ExtractSubjectType<Subjects>
+    })
   }
 
   private createRoleChecker({ roles }: RolesOwner) {
