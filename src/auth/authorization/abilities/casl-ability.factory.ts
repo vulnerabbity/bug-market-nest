4ca37d72c266318d46a1b8dcc209d2e12a28c623
Your Name <you@example.com>
2022-03-04 20:43:18 +0700

    add category support

--- src/auth/authorization/abilities/casl-ability.factory.ts
+++ src/auth/authorization/abilities/casl-ability.factory.ts
@@ -6,10 +6,12 @@ import {
   AbilityClass
 } from "@casl/ability"
 import { Injectable } from "@nestjs/common"
+import { Category } from "src/categories/category.entity"
+import { Product } from "src/products/product.entity"
 import { User, UserRole } from "src/users/user.entity"
 import { RolesOwner } from "./abilities.interface"
 
-type Subjects = InferSubjects<typeof User> | "all"
+type Subjects = InferSubjects<typeof User | typeof Product | typeof Category> | "all"
 
 export type Action = "manage" | "create" | "read" | "update" | "delete"
 
@@ -25,11 +27,20 @@ export class CaslAbilityFactory {
 
     const hasRole = this.createRoleChecker(rolesOwner)
 
+    if (hasRole("super admin")) {
+      can("manage", "all")
+    }
+
+    // TODO: when admin creation will be implemented
+    // restrict admins to manage admins
     if (hasRole("admin")) {
       can("manage", "all")
     }
+
     if (hasRole("seller")) {
       can("manage", User, { id: rolesOwner.id }).because("You can only can manage yourself")
+      can("read", Product)
+      can("manage", Product, { userId: rolesOwner.id }).because("You can only manage your products")
     }
 
     return build({
