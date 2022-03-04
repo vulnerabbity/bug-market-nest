048f6f87e2dd53d80b1726dbdfcb331b6b7c4730
Your Name <you@example.com>
2022-03-04 20:46:32 +0700

    merge inputs in single file

--- src/products/dto/input/create-product.input.ts
+++
@@ -1,9 +0,0 @@
-import { Args, InputType, OmitType } from "@nestjs/graphql"
-import { ProductInput } from "./product.input"
-
-@InputType()
-export class CreateProductInput extends OmitType(ProductInput, []) {}
-
-export function CreateProductArgs() {
-  return Args("createProductInput")
-}
