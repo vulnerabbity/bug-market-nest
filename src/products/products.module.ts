403859d82e122f7c743b00b6c115124653cb076c
Your Name <you@example.com>
2022-03-04 20:46:10 +0700

    add categories support

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,18 +1,16 @@
-import { Module } from "@nestjs/common"
+import { forwardRef, Module } from "@nestjs/common"
 import { MongooseModule } from "@nestjs/mongoose"
 import { AbilitiesModule } from "src/auth/authorization/abilities/abilities.module"
+import { CategoriesModule } from "src/categories/categories.module"
+import { Category, CategorySchema } from "src/categories/category.entity"
 import { Product, ProductSchema } from "./product.entity"
 import { ProductsResolver } from "./products.resolver"
 import { ProductsService } from "./products.service"
 
 @Module({
   imports: [
-    MongooseModule.forFeatureAsync([
-      {
-        name: Product.name,
-        useFactory: ProductSchemaSetup
-      }
-    ]),
+    MongooseModule.forFeatureAsync([{ name: Product.name, useFactory: ProductSchemaSetup }]),
+    MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }]),
     AbilitiesModule
   ],
   providers: [ProductsService, ProductsResolver],
