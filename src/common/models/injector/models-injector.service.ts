cccae4fbc136f957f8592b4b51615f1cc8468f50
Your Name <you@example.com>
2022-03-09 12:38:41 +0700

    add model instances to inject without DI

--- src/common/models/injector/models-injector.service.ts
+++ src/common/models/injector/models-injector.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@nestjs/common"
 import { InjectModel } from "@nestjs/mongoose"
-import { Category, CategoryModel } from "src/categories/category.entity"
-import { PublicFile, PublicFileModel } from "src/files/public/public-file.entity"
-import { Product } from "src/products/product.entity"
+import { Category, categoryModel, CategoryModel } from "src/categories/category.entity"
+import { PublicFile, publicFileModel, PublicFileModel } from "src/files/public/public-file.entity"
+import { Product, productModel } from "src/products/product.entity"
 import { ProductFuzzyModel } from "src/products/products.service"
-import { Session, SessionModel } from "src/sessions/session.entity"
-import { User, UserModel } from "src/users/user.entity"
+import { Session, sessionModel, SessionModel } from "src/sessions/session.entity"
+import { User, userModel, UserModel } from "src/users/user.entity"
 
 /**
  * Allows avoid complex model injection syntax.
@@ -15,17 +15,17 @@ import { User, UserModel } from "src/users/user.entity"
 @Injectable()
 export class ModelsInjectorService {
   @InjectModel(User.name)
-  public userModel!: UserModel
+  public userModel: UserModel = userModel
 
   @InjectModel(Session.name)
-  public sessionModel!: SessionModel
+  public sessionModel: SessionModel = sessionModel
 
   @InjectModel(Product.name)
-  public productModel!: ProductFuzzyModel
+  public productModel: ProductFuzzyModel = productModel
 
   @InjectModel(Category.name)
-  public categoryModel!: CategoryModel
+  public categoryModel: CategoryModel = categoryModel
 
   @InjectModel(PublicFile.name)
-  public publicFileModel!: PublicFileModel
+  public publicFileModel: PublicFileModel = publicFileModel
 }
