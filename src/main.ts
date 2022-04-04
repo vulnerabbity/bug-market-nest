c78b91394ff001c0f1d0a943b4463c5d2e510c33
vulnerabbity <v@example.com>
2022-04-05 03:58:48 +0700

    add slowdown

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import slowDown from "express-slow-down"
+
+// allow 40 requests per minute without delay
+const requestsLimiter = slowDown({
+  windowMs: 60_000,
+  delayAfter: 40,
+  delayMs: 200
+})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -8,6 +16,8 @@ async function bootstrap() {
   // register for "class-validator" library validating
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors()
+  app.use(requestsLimiter)
+
   await app.listen(3000)
 }
 bootstrap()
