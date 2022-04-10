3e8021144aa0c03ce78376914551c7b12fe464b0
vulnerabbity <v@example.com>
2022-04-11 01:19:45 +0700

    add users module

--- src/chat-feature/messages/messages.module.ts
+++ src/chat-feature/messages/messages.module.ts
@@ -1,10 +1,13 @@
-import { Module, Provider } from "@nestjs/common"
+import { Global, Module, Provider } from "@nestjs/common"
+import { UsersModule } from "src/users/users.module"
 import { ChatMessagesResolver } from "./messages.resolver"
 import { ChatMessagesService } from "./messages.service"
 
 const providers: Provider[] = [ChatMessagesResolver, ChatMessagesService]
 
+@Global()
 @Module({
+  imports: [UsersModule],
   providers
 })
 export class ChatMessagesModule {}
