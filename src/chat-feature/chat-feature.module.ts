b125d85c8e5c36d818d73a83af05dd93b7cf47de
vulnerabbity <v@example.com>
2022-04-16 20:01:32 +0700

    add websocket notifications

--- src/chat-feature/chat-feature.module.ts
+++ src/chat-feature/chat-feature.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common"
 import { ChatsModule } from "./chats/chats.module"
 import { ChatMessagesModule } from "./messages/messages.module"
+import { ChatNotificationsModule } from "./notifications/notification.module"
 
-const imports = [ChatsModule, ChatMessagesModule]
+const imports = [ChatsModule, ChatMessagesModule, ChatNotificationsModule]
 
 @Module({
   imports
