b05a6e34a95f8db2b86624321f29b08f1f842d98
vulnerabbity <v@example.com>
2022-04-17 18:54:37 +0700

    add message viewed event

--- src/chat-feature/messages/messages.gateway.ts
+++ src/chat-feature/messages/messages.gateway.ts
@@ -3,7 +3,8 @@ import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
-  WebSocketServer
+  WebSocketServer,
+  WsException
 } from "@nestjs/websockets"
 import { Request } from "express"
 import { Server } from "socket.io"
@@ -11,6 +12,7 @@ import { AccessTokenPayload } from "src/auth/authentication/authentication.inter
 import { JwtWebsocketsAuthenticationGuard } from "src/auth/authentication/guards/jwt-ws-authentication.guard"
 import { appConfig } from "src/common/config"
 import { WSRequest } from "src/common/decorators/ws/ws-request.decorator"
+import { ChatPermissionsCheckInput, ChatsService } from "../chats/chats.service"
 import { SendChatMessageInput } from "./message.input"
 import { ChatMessagesService } from "./messages.service"
 
@@ -22,7 +24,7 @@ export class ChatMessagesGateway {
   @WebSocketServer()
   wsServer!: Server
 
-  constructor(private messagesService: ChatMessagesService) {}
+  constructor(private messagesService: ChatMessagesService, private chatsService: ChatsService) {}
 
   @UsePipes(new ValidationPipe())
   @SubscribeMessage("sendMessage")
@@ -35,5 +37,26 @@ export class ChatMessagesGateway {
     const { userId: receiverId, text } = messageInput
 
     await this.messagesService.sendMessage({ receiverId, senderId, text })
+  }
+
+  @UsePipes(new ValidationPipe())
+  @SubscribeMessage("markChatViewed")
+  async onMarkChatViewed(@WSRequest() req: Request, @MessageBody("chatId") chatId: string) {
+    const { userId: viewerId }: AccessTokenPayload = req.user as AccessTokenPayload
+
+    const chat = await this.chatsService.findByIdOrFail(chatId)
+    this.failIfViewMessageDenied({ chat, requesterId: viewerId })
+
+    const viewedMessages = await this.messagesService.viewMessages(chat.id, viewerId)
+
+    return viewedMessages
+  }
+
+  private failIfViewMessageDenied(input: ChatPermissionsCheckInput) {
+    try {
+      this.chatsService.failIfViewMessageDenied(input)
+    } catch (err: any) {
+      new WsException(err)
+    }
   }
 }
